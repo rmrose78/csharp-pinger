a01df7b8c6fa8d7dd2012195180f31d80fb2789f
avsav <avsav@DESKTOP-KLOUS9G>
2022-07-15 12:43:33 -0400

    Add project files.

---
+++ Pinger/Program.cs
@@ -0,0 +1,9 @@
+﻿using Pinger;
+using System.Net.NetworkInformation;
+using System.Text;
+
+//Pinging google DNS Server 4.2.2.2
+
+PingService pingService = new PingService();
+pingService.SendPing();
+Console.WriteLine();
